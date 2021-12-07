7732cdcfcd19735ab7121ff50abdae03d021b76d
Yana Nechveeva <79226190+Yana1112pstr@users.noreply.github.com>
2021-12-07 18:55:03 +0200

    my commit

--- src/App.js
+++ src/App.js
@@ -1,55 +1,54 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import FeedbackOptions from "./components/FeedbackOptions/FeedbackOptions";
 import Section from "./components/Section/Section";
 import Statistics from "./components/Statistics/Statistics.js";
 import Notification from "./components/Notification/Notification";
 
-class App extends Component {
-  static defaultProps = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  state = {
-    good: this.props.good,
-    neutral: this.props.neutral,
-    bad: this.props.bad,
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onLeaveFeedback = (option) => {
+    const addFeedback = {
+      good: setGood,
+      neutral: setNeutral,
+      bad: setBad,
+    };
+    addFeedback[option]((prevState) => prevState + 1);
   };
 
-  onLeaveFeedback = (option) => {
-    this.setState((prevState) => ({ [option]: prevState[option] + 1 }));
-  };
+  // onLeaveFeedback = (option) => {
+  //   this.setState((prevState) => ({ [option]: prevState[option] + 1 }));
+  // };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    const positivePercentage = Math.round((good / total) * 100);
-    return (
-      <div>
-        <Section title={"Please, leave feedback"}>
-          <FeedbackOptions
-            options={["good", "neutral", "bad"]}
-            onLeaveFeedback={this.onLeaveFeedback}
+  const total = good + neutral + bad;
+  const positivePercentage = Math.round((good / total) * 100);
+  return (
+    <div>
+      <Section title={"Please, leave feedback"}>
+        <FeedbackOptions
+          options={["good", "neutral", "bad"]}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+
+      <Section title={"Statistics"}>
+        {total ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
           />
-        </Section>
-
-        <Section title={"Statistics"}>
-          {total ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          ) : (
-            <Notification notification="No feedback given" />
-          )}
-        </Section>
-      </div>
-    );
-  }
+        ) : (
+          <Notification notification="No feedback given" />
+        )}
+      </Section>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -3,13 +3,14 @@ import PropTypes from "prop-types";
 import s from "./Statistics.module.css";
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+  const { item } = s;
   return (
     <ul>
-      <li className={s.item}>Good: {good}</li>
-      <li className={s.item}>Neutral: {neutral}</li>
-      <li className={s.item}>Bad: {bad}</li>
-      <li className={s.item}>Total: {total}</li>
-      <li className={s.item}>Positive Feedback: {positivePercentage}%</li>
+      <li className={item}>Good: {good}</li>
+      <li className={item}>Neutral: {neutral}</li>
+      <li className={item}>Bad: {bad}</li>
+      <li className={item}>Total: {total}</li>
+      <li className={item}>Positive Feedback: {positivePercentage}%</li>
     </ul>
   );
 };
